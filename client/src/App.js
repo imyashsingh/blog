15d7c0636f9d952952fe01d421f1a5eaeeff2262
imyashsingh <yash.singh5445@gmail.com>
2023-04-04 17:51:43 +0530

    created homepage outline

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>                 
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://miro.medium.com/v2/resize:fit:828/0*7o1nPtr260CgeJ-Z" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>AI and the American Smile</h2>
+          <p className="info">
+            <a className="author">fake name</a>
+            <time>01-01-2023 16:37:57</time>
+          </p>
+          <p className="summary">Imagine a time traveler journeyed to various times and places throughout human history and showed soldiers and warriors of the periods what a “selfie” is</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://miro.medium.com/v2/resize:fit:828/0*7o1nPtr260CgeJ-Z" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>AI and the American Smile</h2>
+          <p className="info">
+            <a className="author">fake name</a>
+            <time>01-01-2023 16:37:57</time>
+          </p>
+          <p className="summary">Imagine a time traveler journeyed to various times and places throughout human history and showed soldiers and warriors of the periods what a “selfie” is</p>
+        </div>
+      </div> 
+      <div className="post">
+        <div className="image">
+          <img src="https://miro.medium.com/v2/resize:fit:828/0*7o1nPtr260CgeJ-Z" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>AI and the American Smile</h2>
+          <p className="info">
+            <a className="author">fake name</a>
+            <time>01-01-2023 16:37:57</time>
+          </p>
+          <p className="summary">Imagine a time traveler journeyed to various times and places throughout human history and showed soldiers and warriors of the periods what a “selfie” is</p>
+        </div>
+      </div>     
+      
+    </main>
+  );
+}
+
+export default App;
